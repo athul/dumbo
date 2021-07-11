ca069e105336f93dfc2d80f4b90c9523b5bea51e
Athul Cyriac Ajay <athul8720@gmail.com>
2021-07-11 14:39:01 +0530

    feat: Add Editor Support

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"text/template"
 	"time"
 )
@@ -18,9 +19,15 @@ var tempFS embed.FS
 
 func main() {
 	title_arg := flag.String("n", time.Now().Format("Monday")+"'s Log", "Create a New Entry to the Journal")
+	goto_arg := flag.Bool("g", false, "Go to Journal Folder")
 	flag.Parse()
 	tempMeta := genMeta(*title_arg)
+	if *goto_arg {
+		tempMeta.GotoFolder()
+		os.Exit(0)
+	}
 	tempMeta.GenTemplate()
+	tempMeta.OpenEditor()
 }
 
 // TemplateMeta holds the MetaData to be prefilled in to Template.md
@@ -40,7 +47,7 @@ func genMeta(title string) TemplateMeta {
 }
 
 // GenTemplate generates the Template from template.md
-// and makes a new folder called Journal. It creates a new file
+// and makes a new folder called .journal in the home dir. It creates a new file
 // with the date as a name and fills in with the data from the template
 func (t *TemplateMeta) GenTemplate() {
 	var b bytes.Buffer
@@ -49,14 +56,35 @@ func (t *TemplateMeta) GenTemplate() {
 		log.Println("Markdown File Opening Error", err)
 	}
 	templ.Execute(&b, t)
-	if _, err := os.Stat("journal"); err != nil {
-		if os.IsNotExist(err) {
-			if err := os.Mkdir("journal", 0755); err != nil {
+	home := os.Getenv("HOME")
+	if _, err := os.Stat(home + "/.journal/"); err != nil {
+		if os.IsExist(err) {
+			if err := os.Mkdir(home+"/.journal", 0755); err != nil {
 				log.Println("Folder Creating Error", err)
 			}
 		}
 	}
 
-	ioutil.WriteFile("journal/"+t.Date+".md", b.Bytes(), 0644)
-	log.Println("\n", b.String())
+	ioutil.WriteFile(home+"/.journal/"+t.Date+".md", b.Bytes(), 0644)
+	log.Println("Journal File Created\n", b.String())
+}
+
+// OpenEdior opens Nvim to edit the journal file
+func (t *TemplateMeta) OpenEditor() {
+	home := os.Getenv("HOME")
+	cmd := exec.Command("nvim", home+"/.journal/"+t.Date+".md")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Println("Cannot Open Neovim", err)
+	}
+}
+
+func (t *TemplateMeta) GotoFolder() {
+	home := os.Getenv("HOME")
+	cmd := exec.Command("cd", home+"/.journal/")
+	if err := cmd.Run(); err != nil {
+		log.Println("Cannot goto to folder", err)
+	}
 }
